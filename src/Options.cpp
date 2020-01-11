b2c8adb1ebfe45e189d7c3a897990f5c985497af
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-01-11 11:23:50 -0500

    Fix indentation

--- src/Options.cpp
+++ src/Options.cpp
@@ -98,7 +98,7 @@ Options::Options(int argc, char* argv[])
         } else if (!strcmp(opt, "-screen-height")) {
             screen_height = get_pos_num_arg(argv, i);
         } else if (!strcmp(opt, "-pixel-size")) {
-			int size = get_pos_num_arg(argv, i);
+            int size = get_pos_num_arg(argv, i);
             pixel_width = size;
             pixel_height = size;
         } else if (!strcmp(opt, "-pixel-width")) {
